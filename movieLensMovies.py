49b0b34d77e477cb8c4407eb957c6ac9ba458961
MrHohn <zzhonzi@gmail.com>
2016-04-05 09:52:53 -0400

    [Parser] Added parser for MovieLens movies.csv

---
+++ movieLensMovies.py
@@ -0,0 +1,61 @@
+import pymongo
+import time
+
+# Parsing of MovieLens movies.csv file.
+# db name: movieLens
+# collection name: movie
+# Adds fields to collection:
+#       -movie id           (mid)
+#       -movie title        (title)
+
+def parse(mongo):
+    progressInterval = 10000  # How often should we print a progress report to the console?
+    progressTotal = 34209     # Approximate number of total lines in the file.
+    bulkSize = 2000           # How many documents should we store in memory before inserting them into the database in bulk?
+    # List of documents that will be given to the database to be inserted to the collection in bulk.
+    bulkPayload = pymongo.bulk.BulkOperationBuilder(mongo.db["movie"], ordered = False)
+    count = 0
+    skipCount = 0
+    bulkCount = 0
+
+    print("[movieLensMovies] Starting Parse of movies.csv")
+    startTime = time.time()
+
+    # open the ratings.csv and gather user's ratings into a list
+    inCSV = open("movielensdata/movies.csv", "r", encoding = "utf8")
+    # the first line is for attributes
+    attrLine = inCSV.readline()
+
+    # save all data in dict
+    # output the data into MongoDB
+    while 1:
+        line = inCSV.readline()
+        if not line:
+            break
+
+        count += 1
+        if count % progressInterval == 0:
+            print("[movieLensMovies] " + str(count) + " lines processed so far. (" + str(int(count * 100 / progressTotal)) + "%%) (%0.2fs)" % (time.time() - startTime))
+
+        curAttrs = line.split(",")
+        mid = int(curAttrs[0])
+        title = curAttrs[1]
+        bulkPayload.find( {"mid": mid} ).update({"$set": { "title": title}})
+        bulkCount += 1
+        if bulkCount >= bulkSize:
+            try:
+                bulkPayload.execute()
+            except pymongo.errors.OperationFailure as e:
+                skipCount += len(e.details["writeErrors"])
+            bulkPayload = pymongo.bulk.BulkOperationBuilder(mongo.db["movie"], ordered = False)
+            bulkCount = 0
+    if bulkCount >= 0:
+        try:
+            bulkPayload.execute()
+        except pymongo.errors.OperationFailure as e:
+            skipCount += len(e.details["writeErrors"])
+
+    print("[movieLensMovies] Parse Complete (%0.2fs)" % (time.time() - startTime))
+    print("[movieLensMovies] Found " + str(count) + " movies.")
+    print("[movieLensMovies] Skipped " + str(skipCount) + " insertions.")
+
--- movieLensParser.py
+++ movieLensParser.py
@@ -1,22 +1,23 @@
 import DataService
 import movieLensRatings
 import movieLensLinks
-import movieLensLikeDislike
+import movieLensMovies
 
 def parseMovieLens():
     mongo = DataService.Mongo("movieLens")
 
     # Add all user ratings into database.
-    # runtime: without bulk (6m12s)
-    # runtime: with bulk size 2000 (172.95s)
+    # runtime: (172.95s)
     movieLensRatings.parse(mongo)
 
     # Add movie id and imdb id pair into database.
     # runtime: (0.93s)
     movieLensLinks.parse(mongo)
 
-    # Add user like and dislike into database. --- unnecessary
-    # movieLensLikeDislike.parse(mongo)
+    # Add movie titles into database.
+    # runtime: (3.15s)
+    movieLensMovies.parse(mongo)
+
 
 def main():
     parseMovieLens()
