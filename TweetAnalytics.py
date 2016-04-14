9519814af35c157b0fb60bf3e2e45f035889a885
MrHohn <zzhonzi@gmail.com>
2016-04-14 14:31:24 -0400

    [Twitter] Finished the prototype of sentiment estimation

---
+++ TweetAnalytics.py
@@ -0,0 +1,69 @@
+import DataService
+import pymongo
+import time
+import re
+import math
+
+# Sentiment degree calculation is referring Visualizing Twitter Sentiment from NCSU
+# https://www.csc.ncsu.edu/faculty/healey/tweet_viz/
+
+class Sentiment(object):
+
+    @classmethod
+    def __init__(self):
+        mongo = DataService.Mongo("anew")
+        anewDoc = mongo.db["list"].find_one({"type": "all"})
+        self.anewDict = anewDoc["dict"]
+        print("[Sentiment] ANEW list retrieved.")
+
+    @classmethod
+    def gain_sentiment(self, sentence):
+        words = tokenize(sentence)
+        if len(words) < 2:
+            return 0
+        valence_list = []
+        for word in words:
+            if word in self.anewDict:
+                valence_list.append([self.anewDict[word]["valence_mean"], self.anewDict[word]["valence_sd"]])
+
+        if len(valence_list) < 2:
+            return 0
+
+        weight_sum = 0
+        value_sum = 0
+        for cur in valence_list:
+            cur_weight = self.probability_density(cur[1])
+            weight_sum += cur_weight
+            value_sum += cur[0] * cur_weight
+        return value_sum / weight_sum
+
+    @classmethod
+    # https://en.wikipedia.org/wiki/Probability_density_function
+    def probability_density(self, sd):
+        return 1 / (sd * math.sqrt(2 * math.pi))
+
+# unfinished
+def tokenize(sentence):
+    words = re.split("\s|\.|;|,|\*|\n", sentence)
+    # print(words)
+    res = []
+    for word in words:
+        if word != " ":
+            res.append(stemming(word))
+    return res
+
+# unfinished
+# handle empty or invalid word, and stem word
+def stemming(word):
+    return word
+
+
+def main():
+    sentiment = Sentiment()
+    sentence = "Congrats to @HCP_Nevada on their health care headliner win"
+    print("[TweetAnalytics] Evaluating sentence: " + sentence)
+    score = sentiment.gain_sentiment(sentence)
+    print("[TweetAnalytics] Sentiment score: " + str(score))
+
+if __name__ == "__main__":
+    main()
