747ec87175aec58fdafce8be51eca11f88e6e679
AndrewFM <AndrewFMs@gmail.com>
2016-04-15 05:15:11 -0400

    [GUI] Auto-download movie cover art from OMDB.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json
+import os.path
+import requests
+import threading
+import urllib.request
 import tkinter as tk
 from PIL import Image, ImageTk
 
@@ -15,7 +20,7 @@ class MovieApp:
 		recommendations = list()
 		recommendations.append("Toy Story (1995)")
 		recommendations.append("Harry Potter and the Sorcerer's Stone (2001)")
-		recommendations.append("Tonari no Totoro (1988)")
+		recommendations.append("My Neighbor Totoro (1988)")
 		recommendations.append("The Matrix (1999)")
 		recommendations.append("Star Wars (1977)")
 		self.recommendations_screen(recommendations)
@@ -74,13 +79,15 @@ class MovieApp:
 		button.pack(side=tk.BOTTOM)
 
 	def movie_frame(self, frame, title, numTitles):
-		movieframe = tk.Frame(frame, width=int(700/numTitles), height=250)
+		WIDTH = int(700/numTitles)
+		HEIGHT = 250
+		movieframe = tk.Frame(frame, width=WIDTH, height=HEIGHT)
 		movieframe.configure(background=BGCOLOR)
 		movieframe.pack(side=tk.LEFT)
 		movieframe.pack_propagate(False)
 
-		image = Image.open("img/blank.jpg")
-		resized = image.resize((int(700/numTitles), 200), Image.ANTIALIAS)
+		image = Image.open("img/loading.jpg")
+		resized = image.resize((WIDTH, HEIGHT-50), Image.ANTIALIAS)
 		art = ImageTk.PhotoImage(resized)
 		coverart = tk.Label(movieframe, image=art)
 		coverart.image = art
@@ -88,11 +95,52 @@ class MovieApp:
 		coverart.pack(side=tk.TOP)
 		coverart.pack_propagate(False)
 
-		label = tk.Label(movieframe, text=title, wraplength=int(700/numTitles))
+		label = tk.Label(movieframe, text=title, wraplength=WIDTH)
 		label.configure(background=BGCOLOR)
 		label.pack(side=tk.BOTTOM)
 
+		thr = threading.Thread(target=self.download_coverart, args=(coverart, title, WIDTH, HEIGHT-50))
+		thr.start()
 
+	def download_coverart(self, label, title, width, height):
+		response = -1
+		foundImage = False
+		titleName = title
+		if "(" in title:
+			titleName = title[:title.index("(")-1]
+		localpath = "img/"+self.safeFileString(titleName)+".jpg"
+
+		if os.path.isfile(localpath):
+			foundImage = True #Cover art exists locally (we've downloaded it on a previous run). Use the local copy.
+
+		if not foundImage:
+			# New cover art we haven't encountered before. Try to download it, if it exists.
+			if "(" in title:
+				titleYear = title[title.index("(")+1:title.index(")")]
+				response = requests.get("http://www.omdbapi.com/?t="+titleName.replace(" ","+")+"&y="+titleYear+"&r=json")
+			else:
+				response = requests.get("http://www.omdbapi.com/?t="+titleName.replace(" ","+")+"&r=json")
+
+			result = json.loads(response.text)
+			if "Poster" in result:
+				coverart = urllib.request.urlretrieve(result["Poster"], localpath)
+				foundImage = True
+
+		if not foundImage:
+			localpath = "img/blank.jpg"
+			foundImage = True
+
+		# If cover art found, set it as the new image for the label.
+		if foundImage:
+			image = Image.open(localpath)
+			resized = image.resize((width, height), Image.ANTIALIAS)
+			art = ImageTk.PhotoImage(resized)
+			label.configure(image=art)
+			label.image=art
+
+	def safeFileString(self, filename):
+		keepcharacters = (' ','.','_')
+		return "".join(c for c in filename if c.isalnum() or c in keepcharacters).rstrip()
 
 
 root = tk.Tk()
